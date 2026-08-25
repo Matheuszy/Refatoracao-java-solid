85426a36b48604d5e1b1467a28c445d06f88896b
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-08-25 16:37:16 -0300

    add Mapper class for JSON mapping functionality

---
+++ src/main/java/br/com/alura/config/Mapper.java
@@ -0,0 +1,17 @@
+package br.com.alura.config;
+
+import br.com.alura.domain.Abrigo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+public class Mapper {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+    public <T> T mapear(String json, Class<T> classeAlvo) {
+        try {
+            return mapper.readValue(json, classeAlvo);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("Erro ao converter JSON para a classe: " + classeAlvo.getName(), e);
+        }
+    }
+}
