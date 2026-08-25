1aff258db3841b0bd297dd84a13fca432c235ca6
Matheuszy <matheusalmeidacarlos@gmail.com>
2026-08-25 18:34:01 -0300

    remove unused imports from AdopetConsoleApplication

--- src/main/java/br/com/alura/AdopetConsoleApplication.java
+++ src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -1,16 +1,10 @@
 package br.com.alura;
-
-import br.com.alura.client.ClientHttpConfiguration;
-import br.com.alura.service.AbrigoService;
-import br.com.alura.service.PetService;
 import java.util.Scanner;
 
 public class AdopetConsoleApplication {
 
     public static void main(String[] args) {
         CommandExecutor commandExecutor = new CommandExecutor();
-        ClientHttpConfiguration client = new ClientHttpConfiguration();
-        PetService petService = new PetService(client);
         System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
         try {
             int opcaoEscolhida = 0;
